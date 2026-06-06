f22cb2045802fd3fb08407d59389188432115987
ddolyniuk1 <58274567+ddolyniuk1@users.noreply.github.com>
2026-06-06 12:42:55 -0400

    fix compile error

--- Terminal.Gui.Reflect.TestApp/Program.cs
+++ Terminal.Gui.Reflect.TestApp/Program.cs
@@ -1,39 +1,47 @@
 ﻿using Terminal.Gui; 
 using Terminal.Gui.Reflect.TestApp;
- 
-ConfigurationManager.Apply();
-ThemeManager.Instance.Theme = "Dark";
 
-Application.Run<ExampleWindow>();
-
-Application.Shutdown();
-
-public class ExampleWindow : Window
+public static class Program
 {
-    public ExampleWindow()
-    { 
-        VerticalScrollBar.AutoShow = true;
-        Title = "Example App (Ctrl+Q to quit)";
-        var model = new BasicViewModel();
-        // var settings = new PropertyGridSettings()
-        // {
-        //     ShowBorder = false,
-        // };
-        Task.Run(() =>
-        { 
-            model.SomeBool2 = true;
-        });
+    public static void Main()
+    {
         
-        // var reflected = new PropertyGrid(model, settings);
-        // reflected.Width  = Dim.Fill();
-        // reflected.Height = Dim.Auto();
-        //
-        // VerticalScrollBar.ScrollableContentSize = 55;
-        //
-        // Add(reflected);
-        model.PropertyChanged += (sender, args) =>
-        {
-            Title = model.SomeText + " " + model.SomeBool;
-        };
     }
 }
+// //  
+// // ConfigurationManager.Apply();
+// // ThemeManager.Instance.Theme = "Dark";
+// //
+// // Application.Run<ExampleWindow>();
+// //
+// // Application.Shutdown();
+//
+// public class ExampleWindow : Window
+// {
+//     public ExampleWindow()
+//     { 
+//         VerticalScrollBar.AutoShow = true;
+//         Title = "Example App (Ctrl+Q to quit)";
+//         var model = new BasicViewModel();
+//         // var settings = new PropertyGridSettings()
+//         // {
+//         //     ShowBorder = false,
+//         // };
+//         Task.Run(() =>
+//         { 
+//             model.SomeBool2 = true;
+//         });
+//         
+//         // var reflected = new PropertyGrid(model, settings);
+//         // reflected.Width  = Dim.Fill();
+//         // reflected.Height = Dim.Auto();
+//         //
+//         // VerticalScrollBar.ScrollableContentSize = 55;
+//         //
+//         // Add(reflected);
+//         model.PropertyChanged += (sender, args) =>
+//         {
+//             Title = model.SomeText + " " + model.SomeBool;
+//         };
+//     }
+// }
